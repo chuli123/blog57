ef43e8c5c5fce892e01a3652b1ee1b5b6c46d13e
token <123@qq.com>
2018-12-29 17:02:32 +0800

    修改关联

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -7,5 +7,29 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
-    protected $fillable = ['tag', ''];
+    protected $fillable = ['tag', 'title', 'subtitle', 'page_image', 'meta_description','reverse_direction'];
+
+    /**
+     * 定义文章与标签之间多对多关联关系
+     *
+     * @return BelongsToMany
+     */
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class, 'post_tag_pivot');
+    }
+
+    /**
+     * Add any tags needed from the list
+     *
+     * @param array $tags List of tags to check/add
+     */
+    public static function addNeededTags(array $tags)
+    {
+        if (count($tags) === 0) {
+            return ;
+        }
+
+        $found = static::whereIn('tag', $tags)->get()->pluck('tag')->all();
+    }
 }
